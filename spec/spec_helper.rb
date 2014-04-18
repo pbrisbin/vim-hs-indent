ff6c0f45eda9cb1e9043e4b30f156f344580c11a
patrick brisbin <pbrisbin@gmail.com>
2014-04-18 16:08:59 -0400

    Initial commit
    - Vimrunner infrastructure
    - Empty indent file

---
+++ spec/spec_helper.rb
@@ -0,0 +1,21 @@
+require 'rspec'
+require 'vimrunner'
+require 'vimrunner/rspec'
+
+Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
+
+ROOT = File.expand_path('../..', __FILE__)
+
+Vimrunner::RSpec.configure do |config|
+  config.reuse_server = true
+  config.start_vim do
+    vim = Vimrunner.start
+    vim.append_runtimepath(ROOT)
+
+    vim
+  end
+end
+
+RSpec.configure do |config|
+  config.order = :random
+end
